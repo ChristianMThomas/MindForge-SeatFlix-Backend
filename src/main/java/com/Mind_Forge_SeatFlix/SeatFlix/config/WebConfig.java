42b8f3f58aea7a2fb0dee5401a9c9baf7ebd9b36
Christian <ChrisThomas0634@gmail.com>
2025-06-17 10:56:26 -0400

    Attempting to fix Uploads & chnaged repo location

--- src/main/java/com/Mind_Forge_SeatFlix/SeatFlix/config/WebConfig.java
+++ src/main/java/com/Mind_Forge_SeatFlix/SeatFlix/config/WebConfig.java
@@ -17,6 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
