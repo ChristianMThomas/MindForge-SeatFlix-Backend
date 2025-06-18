5bef848e46f035f495fda44428c8723c1c1dc1a6
Christian <ChrisThomas0634@gmail.com>
2025-06-18 16:56:50 -0400

    Updated Api security Attempt_6

--- src/main/java/com/Mind_Forge_SeatFlix/SeatFlix/Users/Users_Controller.java
+++ src/main/java/com/Mind_Forge_SeatFlix/SeatFlix/Users/Users_Controller.java
@@ -145,4 +145,9 @@ public class Users_Controller {
         }
     }
 
+    @GetMapping("/whoami")
+    public ResponseEntity<?> whoAmI(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(userDetails != null ? userDetails.getUsername() : "Anonymous");
+    }
+
 }
